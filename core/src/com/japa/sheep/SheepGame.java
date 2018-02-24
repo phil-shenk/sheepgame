da81dfae9d1f243e601d3af0cfd86da96e7b71b3
UristMcBaggins <shenk.philip@gmail.com>
2018-02-24 15:55:13 -0500

    added a sheep pic woohoo we're pretty much doin bois

--- core/src/com/japa/sheep/SheepGame.java
+++ core/src/com/japa/sheep/SheepGame.java
@@ -13,7 +13,7 @@ public class SheepGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("sheep.png");
 	}
 
 	@Override
