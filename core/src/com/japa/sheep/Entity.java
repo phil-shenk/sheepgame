bc6c588fa54ef8f8275ea3400bf9baa97ee15577
Andrew Jia <anj85@pitt.edu>
2018-02-24 17:07:33 -0500

    Entity and position class

---
+++ core/src/com/japa/sheep/Entity.java
@@ -0,0 +1,16 @@
+package com.japa.sheep;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public abstract class Entity {
+    Position pos;
+    Texture skin;
+
+    public abstract Position getPosition();
+
+    public abstract void setPosition();
+
+    public abstract Texture getSkin();
+
+    public abstract void setSkin();
+}
---
+++ core/src/com/japa/sheep/Position.java
@@ -0,0 +1,4 @@
+package com.japa.sheep;
+
+public class Position {
+}
