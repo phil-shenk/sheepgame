d21891dcc778d17ce804888f65a50ceb0051c106
UristMcBaggins <shenk.philip@gmail.com>
2018-02-24 15:01:12 -0500

    Initial commit -everthing

---
+++ desktop/src/com/japa/sheep/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.japa.sheep.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.japa.sheep.SheepGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SheepGame(), config);
+	}
+}
